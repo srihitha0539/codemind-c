4f3c11185e45f8c544640203b80b109bcbeb3116
srihitha0539 <97426083+srihitha0539@users.noreply.github.com>
2022-07-01 19:51:59 +0530

    #include<stdio.h> int main() {     int n;     scanf("%d",&n);     int i,j;     for(i=0;i<n;i++)     {         for(j=0;j<n;j++)         {             if(i==j)             {                 printf("0");             }             else             {                 printf("x");             }         }         printf(" ");     }
    }

---
+++ print_the_following_pattern.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+int main()
+{
+    int n;
+    scanf("%d",&n);
+    int i,j;
+    for(i=0;i<n;i++)
+    {
+        for(j=0;j<n;j++)
+        {
+            if(i==j)
+            {
+                printf("0");
+            }
+            else
+            {
+                printf("x");
+            }
+        }
+        printf("
+");
+    }
+    
+}
